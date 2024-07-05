1a23a46b12a5dc902eb640cebb13f4387cfce34d
bibodev23 <bibodev23@gmail.com>
2024-07-05 20:50:59 +0200

    Initial Commit

---
+++ AimantsAnimaux/AimantsAnimauxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AimantsAnimauxApp.swift
+//  AimantsAnimaux
+//
+//  Created by Boualem Dahmane on 05/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AimantsAnimauxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
