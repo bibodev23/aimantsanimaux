8519e299d141370891570cc369c49bb30bbcee21
bibodev23 <bibodev23@gmail.com>
2024-07-05 21:20:09 +0200

    initalisation projet

--- AimantsAnimaux/AimantsAnimauxApp.swift
+++ AimantsAnimaux/AimantsAnimauxApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AimantsAnimauxApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AnimalsListView()
         }
     }
 }
---
+++ AimantsAnimaux/AnimalDetailView.swift
@@ -0,0 +1,57 @@
+//
+//  AnimalDetailView.swift
+//  AimantsAnimaux
+//
+//  Created by Boualem Dahmane on 05/07/2024.
+//
+
+import SwiftUI
+
+// Affiche le detail des informations pour un animal donné
+struct AnimalDetailView: View {
+    let animal: Animal
+    
+    var body: some View {
+        VStack {
+            Image(systemName: getImageName(breed: animal.breed))
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .foregroundColor(.green)
+                .padding()
+                .clipShape(Circle())
+                .overlay {
+                    Circle().stroke(.green, lineWidth: 5)
+                }
+                .frame(width: 100, height: 100)
+            GroupBox(label: Label("Description", systemImage: "list.clipboard")) {
+                VStack(alignment: .leading) {
+                    LineView(title: "Nom", value: animal.name)
+                    LineView(title: "Race", value: animal.breed.rawValue)
+                    LineView(title: "Age", value: "\(animal.age) an(s)")
+                    LineView(title: "Poids", value: "\(animal.weight) kg")
+                }
+                .padding()
+            }
+            .padding()
+        }
+        Spacer()
+    }
+    
+    // Represente la vue d'une ligne qui permet de visualiser le champs et sa valeur
+    struct LineView : View {
+        let title: String
+        let value: String
+        
+        var body: some View {
+            HStack {
+                Text("\(title) : ")
+                Spacer()
+                Text(value)
+            }
+        }
+    }
+}
+
+#Preview {
+    AnimalDetailView(animal: animalList[0])
+}
